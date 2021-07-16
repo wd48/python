0ea5e5d08fe71b6eb1eba7f0b8a9619af09ff2fb
kastrio_it <wofyd465@gmail.com>
2021-07-17 01:50:39 +0900

    버블 map 객체 선언

--- 3_bubble.py
+++ 3_bubble.py
@@ -10,6 +10,29 @@ class Bubble(pygame.sprite.Sprite):
         self.color = color
         self.rect = image.get_rect(center=position)
 
+# 맵 만들기
+# 전역공간으로 만들어서 다 쓸수 있게
+def setup():
+    global map
+    map = [        
+        #["R","R","Y","Y","B","B","G","G"]
+        # list() : 문자열이 하나하나로 나눠진다!
+        list("RRYYBBGG"),
+        list("RRYYBBG/"), # / : 버블이 위치할 수 없는 곳
+        list("BBGGRRYY"),
+        list("BGGRRYY/"),
+        list("........"),
+        list("......./"),
+        list("........"),
+        list("......./"),
+        list("........"),
+        list("......./"),
+        list("........")
+        # 맵 크기 8*11
+
+    ]
+
+
 pygame.init()
 screen_width = 448
 screen_height = 720
@@ -33,6 +56,8 @@ bubble_images = [
     # black : 화면 전체를 감싸는 게임종료시키는 용도
     pygame.image.load(os.path.join(current_path, "black.png")).convert_alpha()
 ]
+
+map = [] # 맵
 
 # 참일동안 계속 계임 실행
 running = True
