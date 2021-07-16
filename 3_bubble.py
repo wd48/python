2087db2bc51f074fdce81a324e45e9eafa1cb6c9
kastrio_it <wofyd465@gmail.com>
2021-07-17 01:45:10 +0900

    버블 이미지 생성, 선언, 버블 클래스 선언

---
+++ 3_bubble.py
@@ -0,0 +1,49 @@
+# 버블 이미지 설정 / map 생성
+import os
+import pygame
+
+# 버블 클래스 생성
+class Bubble(pygame.sprite.Sprite):
+    def __init__(self, image, color, position):
+        super().__init__()
+        self.image = image
+        self.color = color
+        self.rect = image.get_rect(center=position)
+
+pygame.init()
+screen_width = 448
+screen_height = 720
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Puzzle Bobble")
+clock = pygame.time.Clock()
+
+# 배경이미지 불러오기
+# 실행하는 파일의 경로를 받아올것임 = PBW 나옴
+current_path = os.path.dirname(__file__)
+background = pygame.image.load(os.path.join(current_path, "background.png"))
+
+# 버블 이미지 불러오기
+bubble_images = [
+    # 충돌체크용 convert_alpha
+    pygame.image.load(os.path.join(current_path, "red.png")).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, "yellow.png")).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, "blue.png")).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, "green.png")).convert_alpha(),
+    pygame.image.load(os.path.join(current_path, "pupple.png")).convert_alpha(),
+    # black : 화면 전체를 감싸는 게임종료시키는 용도
+    pygame.image.load(os.path.join(current_path, "black.png")).convert_alpha()
+]
+
+# 참일동안 계속 계임 실행
+running = True
+while running:
+    clock.tick(60) # FPS 60
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    # x,y 0,0 좌표로 넣기
+    screen.blit(background, (0,0))
+    pygame.display.update()
+
+pygame.quit()
