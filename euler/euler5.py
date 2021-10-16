e50d5858038b379aa04b39b18c9f85f1e3dfeb87
kastrio_it <wofyd465@gmail.com>
2021-10-16 23:28:18 +0900

    5번 풀이중 2번

--- euler/euler5.py
+++ euler/euler5.py
@@ -5,12 +5,37 @@
 # GCD : 최대공약수
 
 # 1. math 이용
-import math
+# import math
 
-LCM = 1
+# LCM = 1
 
-for i in range(1, 21):
-    GCD = math.gcd(i, LCM)
-    LCM = int(i * LCM/GCD)
-    print(i, LCM)
+# for i in range(1, 21):
+#     GCD = math.gcd(i, LCM)
+#     LCM = int(i * LCM/GCD)
+#     print(i, LCM)
 
+# 2
+# n=1부터 시작해서 1~20 사이의 수를 나눠서 0을 만족하는 수를 나오게 함
+
+n = 1
+
+# a : 1부터 20까지의 수를 나눠서 나눈 나머지가 0인 c를 담는다
+# b :
+a = list()
+b = list()
+
+while True:
+    print(n)
+    for i in range(1,21):
+        c = n%i 
+        # c: n을 1~20까지 반복하면서 나눈 나머지를 본다
+        zero = 0
+        a.append(c)
+        b.append(zero)
+    
+    if (a==b) == True:
+        print("answer: ",n)
+        break
+    a.clear()
+    b.clear()
+    n += 1
