20f29e59d47a99fb746951abb34b0a72ba710743
kastrio_it <wofyd465@gmail.com>
2021-10-20 23:47:55 +0900

    euler #7 풀이2, 완료

--- euler/euler7.py
+++ euler/euler7.py
@@ -31,3 +31,67 @@ start_t = time()
 li = eratos(150000)
 
 print(time() - start_t)
+
+# 체 이용 코드 2
+
+number = 50
+for i in range(1, number+1):
+    if number%i == 0: # 나눠서 0이면 약수
+        print(i)
+
+# 소수 확인
+# 나누어떨어지는 수가 하나라도 있으면 소수가 아니고 정지
+number = 50
+i=1
+while i <= number:
+    if number%i == 0:
+        print("not prime")
+        break
+    i+=1
+
+# 소수인지에 따라 T/F 반환시키는 함수
+def is_prime(num):
+    if num==1:
+        return False
+    i=2
+    while i<num:
+        if num%i == 0: # 나눠떠러진다 > 소수 아님 > 스탑
+            return False
+        i += 1
+    return True
+
+number = 10000
+
+if is_prime(number):
+    print("prime")
+else:
+    print("not prime")
+
+# 합친 코드
+def is_prime(num):
+    if num == 1:
+        return False
+    i = 2
+    while i<num:
+        if num%i == 0:
+            return False
+        i += 1
+    return True
+# 참일때
+i = 1
+pCnt = 0
+while True:    
+    if is_prime(i):
+        pCnt += 1
+        print(i," is prime factor")
+    if pCnt == 10001:
+        break
+    i += 1
+
+number = 8584038
+i = 1
+while i <= number:
+    # number랑 i가 나눠떨어지고 is_prime 이 참일때
+    if number%i == 0 and is_prime(i):
+        print(i," is prime factor")
+    i += 1
