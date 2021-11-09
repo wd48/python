418e8040f17f1687d58f7362497bc723ea374c34
kastrio_it <wofyd465@gmail.com>
2021-11-09 23:57:37 +0900

    #14 풀이중

--- euler/euler14.py
+++ euler/euler14.py
@@ -84,3 +84,25 @@ while i<100:
     i+=1
 
 # 루프 100만까지...
+import time
+start_time = time.time()
+
+def collatz(n):
+    count=1
+    while not n==1:
+        if n%2==0:
+            n=n/2
+        else:
+            n=n*3+1
+        count+=1
+    return count
+
+i=1;maxVal=0
+
+while i<1000000:
+    val = collatz(i)
+    if val>maxVal :
+        maxVal = val
+    i+=1
+print(maxVal)
+print("calculation time: ",time.time()-start_time)
