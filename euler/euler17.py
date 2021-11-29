82ebbdd1edd03129574b6a8055adc545d17065ee
kastrio_it <wofyd465@gmail.com>
2021-11-29 23:53:38 +0900

    euler #17 풀이

--- euler/euler17.py
+++ euler/euler17.py
@@ -109,3 +109,61 @@ for num in numbers_string:
     
 print(letters_cnt) # 21124 
 # print(numbers_string)
+
+# ref. library num2words
+from num2words import num2words
+
+lenth=0
+for i in range(1, 1001):
+    lenth = lenth + len(((num2words(i)).replace(" ","")).replace("-",""))
+print(lenth)
+
+
+# ref. 
+#1~20 -> 각각 대응, 30, 40등은 조합해서 사용
+numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+            'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen',
+            'nineteen', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',
+            'hundred', 'and', 'thousand']                       #len => 30
+sum = 0
+index = [0, 0, 0]
+string = []
+#0~9 합 구하기
+sum_10 = 0
+for i in range(9):
+    sum_10 += len(numbers[i])
+
+for i in range(1000):
+    r = i + 1
+    #index 정하기
+    index = [0, 0, 0]
+    #방법2 : str형 이용
+    k = str(r)
+    for j in range(len(k)):
+        index[2-j] = int(k[-(j + 1)])    
+    
+    #string 초기화
+    string = []
+
+    #1000
+    if r == 1000:       
+        string = numbers[0] + numbers[-1]
+                                      
+    #100, 200, ..., 900
+    elif r % 100 == 0 and r // 100 > 0:
+        string = numbers[index[0] - 1] + numbers[27]
+    #일반 케이스 - 1~ 19
+    elif r % 100 < 20 :
+        string = numbers[(r%100) - 1]
+        if r > 100 :
+            string = numbers[index[0] - 1] + numbers[27] + numbers[28] + string
+    #일반 케이스 - 20 ~ 99
+    else:
+        string = numbers[index[1] + 17]     #십의자리
+        if r % 10 != 0 :
+            string = string + numbers[index[2] - 1]     #일의자리 
+        if r > 100 :
+            string = numbers[index[0] - 1] + numbers[27] + numbers[28] + string
+
+    sum += len(string)
+print('sum :', sum)
--- euler/tempCodeRunnerFile.py
+++ euler/tempCodeRunnerFile.py
@@ -1,60 +1,46 @@
-numbers_string = [None] * 1001 
-numbers_string[1] = 'one' 
-numbers_string[2] = 'two' 
-numbers_string[3] = 'three' 
-numbers_string[4] = 'four' 
-numbers_string[5] = 'five' 
-numbers_string[6] = 'six' 
-numbers_string[7] = 'seven' 
-numbers_string[8] = 'eight' 
-numbers_string[9] = 'nine'
-numbers_string[10] = 'ten' 
-numbers_string[11] = 'eleven' 
-numbers_string[12] = 'twelve' 
-numbers_string[13] = 'thirteen' 
-numbers_string[14] = 'fourteen' 
-numbers_string[15] = 'fifteen' 
-numbers_string[16] = 'sixteen' 
-numbers_string[17] = 'seventeen' 
-numbers_string[18] = 'eighteen' 
-numbers_string[19] = 'nineteen' 
-numbers_string[20] = 'twenty' 
-numbers_string[30] = 'thirty' 
-numbers_string[40] = 'forty' 
-numbers_string[50] = 'fifty' 
-numbers_string[60] = 'sixty' 
-numbers_string[70] = 'seventy' 
-numbers_string[80] = 'eighty' 
-numbers_string[90] = 'ninety' 
-numbers_string[100] = 'one hundred' 
-numbers_string[200] = 'two hundred' 
-numbers_string[300] = 'three hundred' 
-numbers_string[400] = 'four hundred' 
-numbers_string[500] = 'five hundred' 
-numbers_string[600] = 'six hundred' 
-numbers_string[700] = 'seven hundred' 
-numbers_string[800] = 'eight hundred' 
-numbers_string[900] = 'nine hundred' 
-numbers_string[1000] = 'one thousand' 
+numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+            'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen',
+            'nineteen', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',
+            'hundred', 'and', 'thousand']                       #len => 30
+sum = 0
+index = [0, 0, 0]
+string = []
+#0~9 합 구하기
+sum_10 = 0
+for i in range(9):
+    sum_10 += len(numbers[i])
 
-for i in range(20, 100, 10): 
-    for j in range(1, 10): 
-        idx = i + j 
-        numbers_string[idx] = numbers_string[i] + " " + numbers_string[j] 
-        
-for i in range(1, 10): 
-    for j in range(1, 100): 
-        idx = (i * 100) + j 
-        numbers_string[idx] = numbers_string[i] + " hundred and " + numbers_string[j] 
-        
-letters_cnt = 0 
-for num in numbers_string: 
-    if num is None: 
-        continue 
+for i in range(1000):
+    r = i + 1
+    #index 정하기
+    index = [0, 0, 0]
+    #방법2 : str형 이용
+    k = str(r)
+    for j in range(len(k)):
+        index[2-j] = int(k[-(j + 1)])    
     
-    letters = num.replace(" ", "") 
-    letters = letters.replace("-", "") 
-    letters_cnt += len(letters) 
-    
-print(letters_cnt) # 21124 
-# print(numbers_string)
+    #string 초기화
+    string = []
+
+    #1000
+    if r == 1000:       
+        string = numbers[0] + numbers[-1]
+                                      
+    #100, 200, ..., 900
+    elif r % 100 == 0 and r // 100 > 0:
+        string = numbers[index[0] - 1] + numbers[27]
+    #일반 케이스 - 1~ 19
+    elif r % 100 < 20 :
+        string = numbers[(r%100) - 1]
+        if r > 100 :
+            string = numbers[index[0] - 1] + numbers[27] + numbers[28] + string
+    #일반 케이스 - 20 ~ 99
+    else:
+        string = numbers[index[1] + 17]     #십의자리
+        if r % 10 != 0 :
+            string = string + numbers[index[2] - 1]     #일의자리 
+        if r > 100 :
+            string = numbers[index[0] - 1] + numbers[27] + numbers[28] + string
+
+    sum += len(string)
+print('sum :', sum)
