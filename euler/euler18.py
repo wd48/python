4221a5fde9f45a81768764864058876d52c51c4a
kastrio_it <wofyd465@gmail.com>
2021-12-15 23:16:25 +0900

    euler #19 ing

--- euler/euler18.py
+++ euler/euler18.py
@@ -32,3 +32,27 @@ for i, leaves in enumerate(triangle_leaves[-2::-1]):
         triangle_leaves[n-i-2][j] += max(left, right)
 
 print(triangle_leaves[0][0])
+
+# ref
+n=int(input())
+tree=[]
+for _ in range(n):
+    tree.append(list(map(int, input().split())))
+
+dp=[tree[0][0]]
+for i in range(1, n):
+    a=[]
+    for j in range(i+1):
+        if j==0:
+            a.append(dp[0]+tree[i][0])
+            continue
+        elif j==i:
+            a.append(dp[-1]+tree[i][-1])
+            continue
+        if dp[j]>=dp[j-1]:
+            a.append(dp[j]+tree[i][j])
+        else:
+            a.append(dp[j-1]+tree[i][j])
+    dp=a
+
+print(max(dp))
