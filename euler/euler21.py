ebc662ed7b748e950da032502b85be124bd67ec9
kastrio_it <wofyd465@gmail.com>
2023-10-13 00:41:48 +0900

    euler 21번 진약수 구하기

--- euler/euler21.py
+++ euler/euler21.py
@@ -21,6 +21,7 @@ def proper_divisors(n):
     answer = {1}
     for div in range(2, int(n**0.5) + 1):
         q, r = divmod(n, div)
+        # not r : r이 0인 경우
         if not r:
             answer.update([q, div])
     return answer
@@ -30,3 +31,39 @@ if __name__ == "__main__":
     print(f"16의 진약수: {sorted(proper_divisors(16))}")
 
 
+'''
+친화수 확인하기
+  * 먼저 주어진 수(n)의 진약수의 합(m)을 구한다.
+  proper_divisors 함수에서 받은 값을 sum()으로 더한다.
+  * 진약수의 합(m)을 다시 proper_divisors 함수에 넣어 구한 값이 주어진 수(n)와 같으면 친화수이다.
+'''
+def is_amicable(n):
+    """n이 친화수이면 true, 아니면 false를 반환한다."""
+    m = sum(proper_divisors(n))
+    return True if n == sum(proper_divisors(m)) else False
+
+if __name__ == "__main__":
+    print(f"is 220 amicable number? -> {is_amicable(220)}")
+    print(f"is 284 amicable number? -> {is_amicable(284)}")
+
+'''
+반복문으로 풀기
+>> 문제를 해결하려면 10,000까지 계산을 반복하는데, 그 안에는 진약수를 구하기 위해 for문을 또 돌린다.
+계산 횟수를 줄일 방법을 생각해 보자.
+  * 조건에서 친화수 a와 b는 다른 값이므로 a>b 또는 b>a 라는 의미다. 이걸 이용하자
+  * 작은 수부터 검사하므로, 이 수를 a라고 하면 친화수의 쌍인 b는 a보다 커야 한다.
+  * 즉, b>a 일 때만 친화수인지 계산하다. 그리고 b는 10,000 미만이어야 한다.
+  * is_amicable 함수의 코드를 solution 함수에 넣자
+'''
+
+def Q21_v21(n):
+    """n 미만의 친화수를 모두 구하여 더한다."""
+    answer = 0
+    for a in range(2, n):
+        b = sum(proper_divisors(a))
+        if (n > b > a) and a == sum(proper_divisors(b)):
+            answer += (a+b)
+    return answer
+
+if __name__ == "__main__":
+    print(Q21_v21(300))
